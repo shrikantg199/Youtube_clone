cf8b413e0065606cb801dca9340970698d901153
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-14 19:20:29 +0530

    initial commit

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-
+import ButtonList from "./ButtonList";
+import VideoContainer from "./VideoContainer";
 function Feed() {
-  return <div></div>;
+  return (
+    <div className=" mt-5 h-full   ml-[200px] w-96 text-white  ">
+      <ButtonList />
+      <VideoContainer />
+    </div>
+  );
 }
 
 export default Feed;
