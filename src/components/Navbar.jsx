e075d7df8dd67adcd233c9a3f3354fa0ed7ccf45
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-14 19:21:08 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { CiBellOn } from "react-icons/ci";
-import logo from "../assets/images/yt-logo.png";
+
 import { CiSearch } from "react-icons/ci";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import { RiVideoDownloadFill } from "react-icons/ri";
 import { Avatar } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { toggleSidebar } from "../app/slice/appSlice";
+
 function Navbar() {
+  const dispatch = useDispatch();
+  const toggleHandler = () => {
+    dispatch(toggleSidebar());
+  };
   return (
     <div>
-      <div className="flex justify-between items-center sticky top-0">
-        <div className="flex items-center gap-5">
-          <GiHamburgerMenu className="text-4xl font-semibold m-2" />
-          <YouTubeIcon sx={{ fontSize: 52, color: "red" }} />
-          <span className="font-bold text-3xl">Youtube</span>
+      <div className="h-16 top-0 bottom-0 flex fixed justify-between items-center w-full bg-white z-10  ">
+        <div className="flex items-center gap-5  ">
+          <GiHamburgerMenu
+            className="text-4xl font-semibold m-2 cursor-pointer"
+            onClick={toggleHandler}
+          />
+          <div className="flex justify-center items-center">
+            <YouTubeIcon sx={{ fontSize: 52, color: "red" }} />
+
+            <span className="font-bold text-2xl">Youtube</span>
+          </div>
         </div>
+
         <div className="flex">
           {" "}
           <input
             type="text"
-            className="border p-2 py-1 border-gray-500 rounded-s-2xl w-[500px] outline-none"
+            className="border p-2 py-1 border-gray-200 rounded-s-2xl w-[500px] outline-none hidden lg:block"
             placeholder="Search.."
           />
-          <CiSearch className="text-3xl bg-gray-200/50 w-12 rounded-e-2xl text-black h-10" />
+          <CiSearch className="text-xl  bg-gray-300/30 w-16 p-1 rounded-e-2xl text-black h-10 " />
         </div>
-        <div className="flex gap-4 m-3">
-          <CiBellOn className="text-4xl text-black" />
-          <RiVideoDownloadFill className="text-4xl text-black" />
+        <div className="flex gap-4 m-3 items-center">
+          <CiBellOn className="text-3xl text-black" />
+          <RiVideoDownloadFill className="text-3xl text-black" />
           <Avatar
             alt="Remy Sharp"
             src="/static/images/avatar/1.jpg"
-            sx={{ width: 40, height: 40, margin: "2px" }}
+            sx={{ width: 30, height: 30, margin: "2px" }}
+            className=""
           />
         </div>
       </div>
