5de41e6f4ddc964a5a312074df70f0daf96f58b5
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-15 11:15:01 +0530

    initial commit

---
+++ src/app/slice/appSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+const appSlice = createSlice({
+  name: "app",
+  initialState: { open: true },
+  reducers: {
+    toggleSidebar: (state) => {
+      state.open = !state.open;
+    },
+  },
+});
+export const { toggleSidebar } = appSlice.actions;
+export default appSlice.reducer;
