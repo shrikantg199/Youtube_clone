d2457ee18f7ef8c233b6a3bc1f564984ac822048
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-16 10:54:40 +0530

    initial commit

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -3,9 +3,12 @@ import React from "react";
 function ButtonList() {
   const ButtonList = ["All", "javascript", "java", "Video"];
   return (
-    <div className="fixed w-full left-[250px] py-3 top-[60px] bg-white">
+    <div className=" w-full left-[250px] py-2 top-[60px] bg-white fixed h-18 z-10">
       {ButtonList.map((item, index) => (
-        <button key={index} className="px-3 m-2 bg-red-600">
+        <button
+          key={index}
+          className="px-3  py-2 m-3 bg-gray-500/40 text-white rounded-xl "
+        >
           {item}
         </button>
       ))}
