a611c40e7ceabf818e3126cedc901275ab5ae0ee
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-16 10:58:26 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+
 import { Provider } from "react-redux";
 import { store } from "./app/store.js";
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
