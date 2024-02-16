2b4547f5718bdff4bf2f803b33cb46935ff5dd45
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-16 10:56:37 +0530

    initial commit

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { YOUTUBE_VIDEO_API } from "../constant/youtube.js";
 import VideoCard from "./VideoCard.jsx";
+import { Link } from "react-router-dom";
 
 function VideoContainer() {
   const [videos, setVideos] = useState([]);
@@ -9,7 +10,7 @@ function VideoContainer() {
   const fetchingYoutubeVideo = async () => {
     try {
       const { data } = await axios.get(YOUTUBE_VIDEO_API);
-      //console.log(data.items);
+      console.log(data.items);
       setVideos(data?.items);
     } catch (error) {
       console.error("Error fetching YouTube videos:", error);
@@ -21,10 +22,15 @@ function VideoContainer() {
   }, []);
 
   return (
-    <div className="flex flex-wrap  gap-2 py-2 ">
-      {videos.map((item, index) => (
-        <VideoCard key={index} item={item} />
-      ))}
+    <div className="flex mt-8 flex-wrap  gap-2 py-2 ">
+      {videos.map((item, index) => {
+        //console.log(item.id);
+        return (
+          <Link key={index} to={`/watch?v=${item.id}`}>
+            <VideoCard item={item} />
+          </Link>
+        );
+      })}
     </div>
   );
 }
