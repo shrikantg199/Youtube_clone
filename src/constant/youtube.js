963814a80b73b410ef9cbb5f8f588a380ad585d4
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-18 14:24:14 +0530

    initial commit

--- src/constant/youtube.js
+++ src/constant/youtube.js
@@ -1,4 +1,4 @@
 export const API_KEY = "AIzaSyD85U8UIixQEzW44ppOJ2k8rp6EV8PILfk";
 export const BASE_URL = " https://www.googleapis.com/youtube/v3";
 
-export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=100&regionCode=US&key=${API_KEY}`;
+export const YOUTUBE_VIDEO_API = ` https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${API_KEY}`;
