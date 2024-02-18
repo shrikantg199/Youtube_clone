d6eee7f85fa9b45d57e7eb003ddd436d68017e25
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-18 14:22:14 +0530

    initial commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { FaHome } from "react-icons/fa";
 import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
 import { MdOutlineManageHistory } from "react-icons/md";
@@ -8,15 +7,17 @@ import shorticon from "../assets/images/shorticon.png";
 import { CgProfile } from "react-icons/cg";
 
 import { useSelector } from "react-redux";
-import VideoContainer from "./VideoContainer";
-import ButtonList from "./ButtonList";
-import Feed from "./Feed";
+
 import { Link } from "react-router-dom";
 
 const menuItems = [
-  { Link: "/", icon: <FaHome className="text-2xl m-4  " />, label: "Home" },
   {
-    Link: "#",
+    Link: "/",
+    icon: <FaHome className="text-2xl m-4   " />,
+    label: "Home",
+  },
+  {
+    Link: "/shorts",
     icon: <img src={shorticon} className="h-10 w-10 m-2 " alt="" />,
     label: "Shorts",
   },
@@ -46,35 +47,30 @@ const menuItems = [
     label: "Watch Later",
   },
 ];
-
 function Sidebar() {
   const open = useSelector((state) => state.app.open);
-  console.log(open);
 
   return (
-    <>
-      <div className="flex w-screen ">
-        <div
-          className={`bg-white top-16 h-screen w-[16%] z-10  ${
-            !open && "hidden  transition-all"
-          } left-0  flex overflow-y-auto fixed `}
-        >
-          {/*Desktop */}
-          <div className="">
-            {menuItems.map((item, index) => (
-              <Link to={item.Link} key={index}>
-                <div className="flex justify-start items-center">
-                  {" "}
-                  <span className="">{item.icon}</span>
-                  <span className="text-lg ">{item.label}</span>
-                </div>
-              </Link>
-            ))}
-            <hr />
-          </div>
-        </div>
+    <div
+      className={`bg-white top-16 h-screen w-auto z-10 left-0 flex overflow-y-auto fixed  transition-all`}
+    >
+      {/* Desktop */}
+      <div>
+        {menuItems.map((item, index) => (
+          <Link to={item.Link} key={index}>
+            <div className="flex justify-start items-center">
+              <span>{item.icon}</span>
+              {open ? (
+                <p className="block transition-all px-6">{item.label}</p>
+              ) : (
+                <p className="hidden transition-all">{item.label}</p>
+              )}
+            </div>
+          </Link>
+        ))}
+        <hr />
       </div>
-    </>
+    </div>
   );
 }
 
