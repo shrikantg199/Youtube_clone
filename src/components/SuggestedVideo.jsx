d57065ce7c423ff281512fb874e4907c5458ae51
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-18 14:22:35 +0530

    initial commit

--- src/components/SuggestedVideo.jsx
+++ src/components/SuggestedVideo.jsx
@@ -2,11 +2,23 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { API_KEY, YOUTUBE_VIDEO_API } from "../constant/youtube";
 import { formatDistanceToNow } from "date-fns";
+import { Link } from "react-router-dom";
 function SuggestedVideo() {
   const [suggested, setsuggested] = useState([]);
-  const fetchSuggestedVideos = async () => {
+  const fetchSuggestedVideos = async (videoId) => {
     try {
-      const { data } = await axios.get(YOUTUBE_VIDEO_API);
+      const { data } = await axios.get(
+        `https://youtube.googleapis.com/youtube/v3/search`,
+        {
+          params: {
+            part: "snippet",
+            relatedToVideoId: videoId,
+            type: "video",
+            maxResults: 30, // Adjust as needed
+            key: API_KEY,
+          },
+        }
+      );
       console.log(data.items);
       setsuggested(data?.items);
     } catch (error) {
@@ -32,23 +44,27 @@ function SuggestedVideo() {
   };
   return (
     <>
-      <div className=" w-full p-2  h-40 flex-col">
+      <div className=" w-[90%] p-2  h-1/2 flex-col overflow-y-auto ">
         {suggested.map((item, index) => (
-          <div key={index} className="flex m-3">
-            <img
-              src={item?.snippet?.thumbnails?.high?.url}
-              alt=""
-              className="relative h-[100px] w-[150px]  rounded-xl bg-cover "
-            />
-            <div className=" ml-4">
-              <h1 className=" font-bold">{item.snippet.title.slice(0, 30)}</h1>
-              <h1 className="">{item.snippet.channelTitle}..</h1>
-              <div className="flex gap-3">
-                <h1>{formatViews(item.statistics.viewCount)}</h1>
-                <h1> {formatRelativeTime(item.snippet.publishedAt)}</h1>
+          <Link key={index} to={`/watch?v=${item.id.videoId}`}>
+            {console.log(item.id.videoId)}
+            <div className="flex m-3">
+              <img
+                src={item?.snippet?.thumbnails?.high?.url}
+                alt=""
+                className="relative h-[100px] w-[150px]  rounded-xl bg-cover "
+              />
+              <div className=" ml-4">
+                <h1 className=" font-bold">
+                  {item.snippet.title.slice(0, 30)}
+                </h1>
+                <h1 className="">{item.snippet.channelTitle}..</h1>
+                <div className="flex gap-3">
+                  <h1> {formatRelativeTime(item.snippet.publishedAt)}</h1>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </>
