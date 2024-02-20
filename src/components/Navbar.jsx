8ceaec6ca8594376f95b98f0756630fe538c046b
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-20 11:53:28 +0530

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { CiBellOn } from "react-icons/ci";
 import { CiSearch } from "react-icons/ci";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import { RiVideoDownloadFill } from "react-icons/ri";
-import { Avatar } from "@mui/material";
+import { Avatar, Hidden } from "@mui/material";
 import { useDispatch } from "react-redux";
 import { toggleSidebar } from "../app/slice/appSlice";
 
@@ -18,7 +18,7 @@ function Navbar() {
       <div className="h-16 top-0 bottom-0 flex fixed justify-between items-center w-full bg-white z-10  ">
         <div className="flex items-center gap-5  ">
           <GiHamburgerMenu
-            className="text-2xl font-semibold ml-6 cursor-pointer"
+            className="text-xl lg:text-2xl  font-semibold ml-3 lg:ml-6 cursor-pointer"
             onClick={toggleHandler}
           />
           <div className="flex justify-center items-center">
@@ -36,17 +36,18 @@ function Navbar() {
             className="border p-2 py-1 border-gray-200 rounded-s-2xl w-[500px] outline-none hidden lg:block"
             placeholder="Search.."
           />
-          <CiSearch className="text-xl  bg-gray-300/30 w-16 p-1 rounded-e-2xl text-black h-10 " />
+          <CiSearch className="text-xl hidden lg:block  bg-gray-300/30 w-16 p-1 rounded-e-2xl text-black h-10 " />
         </div>
         <div className="flex gap-4 m-3 items-center">
           <CiBellOn className="text-3xl text-black" />
-          <RiVideoDownloadFill className="text-3xl text-black" />
+          <RiVideoDownloadFill className="hidden lg:block text-3xl text-black" />
           <Avatar
             alt="Remy Sharp"
             src="/static/images/avatar/1.jpg"
             sx={{ width: 30, height: 30, margin: "2px" }}
             className=""
           />
+          <CiSearch className="text-xl   lg:hidden w-16 p-1 rounded-e-2xl text-black h-10 " />
         </div>
       </div>
     </div>
