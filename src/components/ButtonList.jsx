e7c13e072e3f412712766488df555b122114b763
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-20 11:53:05 +0530

    initial commit

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ButtonList() {
-  const ButtonList = ["All", "javascript", "java", "Video"];
+  const [active, setActive] = useState("All");
+  const ButtonList = [
+    "All",
+    "javascript",
+    "java",
+    "Video",
+    "news",
+    "cricket",
+    "comedy",
+    "All",
+    "javascript",
+    "java",
+    "Video",
+    "news",
+    "cricket",
+    "comedy",
+    "All",
+    "javascript",
+    "java",
+    "Video",
+    "news",
+    "cricket",
+    "comedy",
+  ];
+  const videoTag = (tag) => {
+    if (active !== tag) {
+      setActive(tag);
+    }
+    console.log(tag);
+  };
   return (
-    <div className=" w-full left-[250px] py-2 top-[60px] bg-white fixed h-18 z-10">
-      {ButtonList.map((item, index) => (
+    <div className="flex w-full left-10  lg:left-[250px] py-2 top-[60px] fixed no-scrollbar h-18 bg-white z-10 overflow-x-scroll sm:overflow-hidden ">
+      {ButtonList.map((buttonName, index) => (
         <button
           key={index}
-          className="px-3  py-2 m-3 bg-gray-500/40 text-white rounded-xl "
+          onClick={() => videoTag(buttonName)}
+          className={`${
+            active === buttonName
+              ? "bg-blue-500 text-white" // Active button color
+              : "bg-gray-500/40 text-white" // Inactive button color
+          } lg:px-3 px-2 py-1 lg:py-2 m-3 bg-gray-500/40 text-white rounded-xl`}
         >
-          {item}
+          <span>{buttonName}</span>
         </button>
       ))}
     </div>
