7ecf75c7af61f7eba01c63290f679226251042aa
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-22 12:08:33 +0530

    initial commit

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setCategory, setHomeVideo } from "../app/slice/appSlice";
 
 function ButtonList() {
   const [active, setActive] = useState("All");
@@ -10,38 +12,38 @@ function ButtonList() {
     "news",
     "cricket",
     "comedy",
-    "All",
-    "javascript",
-    "java",
-    "Video",
-    "news",
-    "cricket",
-    "comedy",
-    "All",
-    "javascript",
-    "java",
-    "Video",
-    "news",
-    "cricket",
-    "comedy",
+    "music",
+    "gaming",
+    "movies",
+    "sports",
+    "education",
+    "science",
+    "technology",
+    "travel",
+    "food",
+    "fitness",
+    "lifestyle",
   ];
+  const dispatch = useDispatch();
   const videoTag = (tag) => {
     if (active !== tag) {
+      dispatch(setCategory(tag));
       setActive(tag);
     }
+    // alert(tag);
     console.log(tag);
   };
   return (
-    <div className="flex w-full left-10  lg:left-[250px] py-2 top-[60px] fixed no-scrollbar h-18 bg-white z-10 overflow-x-scroll sm:overflow-hidden ">
+    <div className="flex items-center py-2 top-[60px] fixed no-scrollbar h-18 bg-white z-10 overflow-x-auto  lg:left-[250px] w-full gap-3">
       {ButtonList.map((buttonName, index) => (
         <button
           key={index}
           onClick={() => videoTag(buttonName)}
           className={`${
             active === buttonName
-              ? "bg-blue-500 text-white" // Active button color
+              ? "bg-blue-700 text-white " // Active button color
               : "bg-gray-500/40 text-white" // Inactive button color
-          } lg:px-3 px-2 py-1 lg:py-2 m-3 bg-gray-500/40 text-white rounded-xl`}
+          } lg:px-3 px-2 py-1 lg:py-2  bg-gray-500/40   text-white rounded-xl`}
         >
           <span>{buttonName}</span>
         </button>
